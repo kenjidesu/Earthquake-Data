96f806ae05227f9932cfe7ba37194a087cf60b88
kenji <rkellazar@gmail.com>
2021-02-23 09:19:57 +0800

    Map Earthquakes

---
+++ eq_explore_data.py
@@ -0,0 +1,33 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+
+# Explore the structure of the data
+filename = 'data/raw_eq_data.json'
+with open(filename, encoding='UTF-8') as f:
+    all_eq_data = json.load(f)
+
+# Loading the data and displaying it in a format that's easier to read
+# readable_file = 'data/readable_eq_data.json'
+# with open(readable_file, 'w') as f:
+#     json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+# Map the earthquakes
+data = [Scattergeo(lon=lons, lat=lats)]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
