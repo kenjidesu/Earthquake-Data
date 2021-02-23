5ff674cf79517fd228eb9aa566c03bcd81649af1
kenji <rkellazar@gmail.com>
2021-02-23 10:41:46 +0800

    Adding Hover Text

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -16,14 +16,16 @@ with open(filename, 'r', encoding='utf-8') as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts= [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 
 mag_size = []
@@ -31,7 +33,7 @@ for mag in mags:
     try:
         result = mag * 5
     except TypeError:
-        print('nope')
+        print('Missing Data')
     else:
         if mag > 0:
             mag_size.append(result)
@@ -42,6 +44,7 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': mag_size,    
         'color': mag_size,
